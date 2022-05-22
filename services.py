616746662869120cc6b65d6cebf84866dbacb278
Kamila <kamkopiczko@gmail.com>
2022-05-22 12:39:44 +0200

    created get_db function that creates database connection

--- services.py
+++ services.py
@@ -1,6 +1,19 @@
 import database as _db
 import models as _models
+from schemas import ContactCreate
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import Session
 
 def _create_database():
     """Creates tables according to the models"""
     return _db.Base.metadata.create_all(bind=_db.engine)
+
+# function that creates database session
+def get_db():
+    db = _db.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
